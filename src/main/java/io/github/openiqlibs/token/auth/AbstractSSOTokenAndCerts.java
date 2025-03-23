83952819542b2c81e33be8961f3830eb1d3c4e61
aditya-deshmukh-tech <asdeshmukh97@gmail.com>
2025-03-23 11:57:54 +0530

    added apache license and notice

--- src/main/java/io/github/openiqlibs/token/auth/AbstractSSOTokenAndCerts.java
+++ src/main/java/io/github/openiqlibs/token/auth/AbstractSSOTokenAndCerts.java
@@ -21,6 +21,19 @@ import java.security.spec.RSAPublicKeySpec;
 import java.util.*;
 
 /**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  * This abstract class is used get token related certs and validation of token.
  * Also contains useful methods for downloading and loading certs to public keys.
  * Needs to extend to use this library.
--- src/main/java/io/github/openiqlibs/token/auth/RoleExtractor.java
+++ src/main/java/io/github/openiqlibs/token/auth/RoleExtractor.java
@@ -5,6 +5,19 @@ import io.jsonwebtoken.Claims;
 import java.util.Set;
 
 /**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  * Interface RoleExtractor used in InAppTokenAndCerts class to extract roles from token
  * Needs to be implemented and passed implemented instance to InAppTokenAndCerts builder method {@code setRoleExtractor}
  */
